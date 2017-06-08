2396a6189a9e84508cca609b89db77aeaccbd3d9
rleandroruiz <leo1998.04.24@hotmail.com>
2017-06-08 10:02:58 -0300

    Se agrego funcion verificar y canvas

--- funciones.js
+++ funciones.js
@@ -63,6 +63,7 @@ function calculos() {
         incognita = "masa";
 
     var v1, v2, v3, u1, u2, u3;
+    var unidad_final;
 
     switch (incognita) {
         case "area":
@@ -73,6 +74,8 @@ function calculos() {
             u1 = document.getElementById("unidadesalt2").value;
             u2 = document.getElementById("unidadesa1").value;
             u3 = document.getElementById("unidadesalt1").value;
+
+            unidad_final = "m2";
 
             break;
         case "altura":
@@ -85,6 +88,8 @@ function calculos() {
             u2 = document.getElementById("unidadesalt1").value;
             u3 = document.getElementById("unidadesm1").value;
 
+            unidad_final = "m";
+
             break;
         case "masa":
 
@@ -96,12 +101,60 @@ function calculos() {
             u2 = document.getElementById("unidadesa2").value;
             u3 = document.getElementById("unidadesa1").value;
 
+            unidad_final = "kg";
+
             break;
     }
 
-    document.getElementById("prueba_resultado").innerHTML = v1 *  factor_conversion(u1) * v2 * factor_conversion(u2)  / (v3 * factor_conversion(u3));
+    document.getElementById("prueba_resultado").innerHTML = v1 * factor_conversion(u1) * v2 * factor_conversion(u2) / (v3 * factor_conversion(u3)) + " " + unidad_final;
+    dibujar();
 }
 
+
+function dibujar() {
+    var canvas = document.getElementById("mycanvas");
+    var ctx = canvas.getContext("2d");
+
+    var ancho = canvas.width;
+    var alto = canvas.height;
+
+    var yo = 20;
+    var h1 = 10;
+    var h2 = -h1;
+
+    //ancho canvas= 900 alto= 440
+
+    ctx.strokeStyle = "#9e9fa6";
+    ctx.fillStyle = "#1d0bff";
+    ctx.lineWidth = "8";
+
+    ctx.beginPath();
+
+    ctx.rect((ancho / 2) - 125, alto - yo - 30, 250, 30);   //base prensa
+
+    ctx.rect((ancho / 2) - 125, alto - yo - 60 - h1, 40, 60 + h1);       //lado izq
+
+    ctx.rect((ancho / 2) + 125 - 60, alto - yo - 60 - h2, 60, 60 + h2);     //lado der
+
+    ctx.stroke();
+    ctx.fill();
+
+    ctx.closePath();
+
+    ctx.strokeStyle = "#fffc4d";
+    ctx.fillStyle = "#ff0000";
+
+    ctx.beginPath();
+
+    ctx.rect((ancho / 2) - 125 + 20 - 10, alto - yo - 90, 20, 20);
+    ctx.rect((ancho / 2) + 125 - 40, alto - yo - 90, 20, 20);
+
+    ctx.stroke();
+    ctx.fill();
+
+
+    ctx.closePath();
+}
 
 function factor_conversion(unidad) {
     if (unidad == "g")
@@ -116,3 +169,32 @@ function factor_conversion(unidad) {
     return 1;
 }
 
+function verificar() {
+    var estado = true;
+    if(document.getElementById("area1").value <= 0)
+        estado = false;
+    if(document.getElementById("masa1").value <= 0)
+        estado = false;
+    if(document.getElementById("altura1").value < 0)
+        estado = false;
+
+    var incognita;
+
+    if (document.getElementById("area").checked)
+        incognita = "area";
+    if (document.getElementById("altura").checked)
+        incognita = "altura";
+    if (document.getElementById("masa").checked)
+        incognita = "masa";
+
+    switch (incognita){
+
+    }
+
+    if(estado == true)
+        calculos();
+    else
+        alert("ESTO NO FUNCA");
+
+
+}
