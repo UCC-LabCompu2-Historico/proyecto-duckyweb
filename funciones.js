81c802ea38ca1fd4ad7614e7754b368998f3eba9
NeloGino <nelogino@gmail.com>
2017-06-29 14:15:04 -0300

    sup 2

--- funciones.js
+++ funciones.js
@@ -97,8 +97,8 @@ function calcular_Masa() {
     m2 = Math.round(m2 * 1000) / 1000;
     pres = Math.round(pres * 1000) / 1000;
 
-    document.getElementById("prueba_resultado").innerHTML = m2 + " m2";
-    document.getElementById("presion_resultado").innerHTML = pres + " N/m2";
+    document.getElementById("prueba_resultado").innerHTML = m2 + " m&sup2";
+    document.getElementById("presion_resultado").innerHTML = pres + " N/m&sup2";
 }
 
 /*
@@ -230,7 +230,7 @@ function dibujar(a1, m1, a2, m2, pres) {
 
         ctx.closePath();
         if (ilatina < p)
-            ilatina ++;
+            ilatina++;
         else
             clearInterval(int);
 
